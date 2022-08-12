bfe24b506a6bee1bc7bfab721e3e7ab5d3c025f3
MaxiSound <maxisound@yandex.ru>
2022-08-12 14:27:33 +0700

    и тут чуть поправил

--- Homework01/second_task/Program.cs
+++ Homework01/second_task/Program.cs
@@ -2,11 +2,14 @@
 
 Console.WriteLine("Введите первое число:");
 int num_1 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите второе число:");
 int num_2 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите третье число:");
 int num_3 = Convert.ToInt32(Console.ReadLine());
 int max = num_1;
+
 if (num_2 > max)
 {
 max = num_2;
