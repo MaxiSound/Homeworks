642dfcd6d6b1d600784e83ee0013922e5886df19
MaxiSound <maxisound@yandex.ru>
2022-08-12 13:51:13 +0700

    Четвёртую задачу выполнил

---
+++ Homework01/fourth_task/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int i = 1;
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + number);
+while (i <= number)
+{
+    if (i % 2 != 1)
+    {
+    Console.Write(i + ", ");
+    }
+i++;
+}
+if (i <= 1)
+{
+Console.WriteLine("Чётных чисел не найдено!");
+}
