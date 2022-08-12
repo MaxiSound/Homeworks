5ae5f41f37493d8bc0f32fdfa2e85b620ff5b33f
MaxiSound <maxisound@yandex.ru>
2022-08-12 15:12:51 +0700

    LONG TIME

--- Homework01/fourth_task/Program.cs
+++ Homework01/fourth_task/Program.cs
@@ -1,19 +1,21 @@
 ﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-int i = 1;
-
 Console.WriteLine("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
 Console.WriteLine("Чётные числа от 1 до " + number);
-while (i <= number)
+
+if (number > 1)
+    while (i <= number)
 {
-    if (i % 2 != 1)
+    if (i % 2 == 0)
     {
     Console.Write(i + ", ");
     }
-i++;
+    i++;
 }
-if (i <= 1)
+if (number <= 1)
 {
-Console.WriteLine("Чётных чисел не найдено!");
+    Console.WriteLine("Чётных чисел не найдено");
 }
