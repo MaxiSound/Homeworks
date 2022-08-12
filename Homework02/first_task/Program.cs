78b4e0bb59387cab4dbc2552141f4ab41139e678
MaxiSound <maxisound@yandex.ru>
2022-08-12 17:52:05 +0700

    Первая задача выполнена

--- Homework02/first_task/Program.cs
+++ Homework02/first_task/Program.cs
@@ -1,3 +1,14 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
 Console.WriteLine("Введите трёхзначное число: ");
-if (num <= 100 | num > 999)
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    number = number % 100 / 10;
+    Console.WriteLine("Вторая цифра введённого числа: " + number);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трёхзначное число, попробуй ещё раз!");
+}
