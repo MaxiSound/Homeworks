36c50e392e7d255cb57f4a118df7c58f8d03c8c6
MaxiSound <maxisound@yandex.ru>
2022-08-19 12:42:42 +0700

    Наконец-то и третью задачу закончил

--- Homework03/third_task/Program.cs
+++ Homework03/third_task/Program.cs
@@ -5,26 +5,26 @@ int cube = Convert.ToInt32(Console.ReadLine());
 
 void Cube(int[] cube)
 {
-  int counter = 1;
-  int length = cube.Length;
-  while (counter <  length)
-  {
-    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-    counter++;
-  }
+    int counter = 1;
+    int length = cube.Length;
+    while (counter < length)
+    {
+        cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
 }
 
 void PrintArry(int[] coll)
 {
-  int counter = coll.Length;
-  int index = 1;
-  while(index < counter)
-  {
-    Console.Write(coll[index]+ " ");
-    index++;
-  }
-} 
+    int counter = coll.Length;
+    int index = 1;
+    while (index < counter)
+    {
+        Console.Write(coll[index] + " ");
+        index++;
+    }
+}
 
-int[] arry = new int[cube+1];
+int[] arry = new int[cube + 1];
 Cube(arry);
 PrintArry(arry);
