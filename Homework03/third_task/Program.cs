ea873ce8dba00160a0146f8ca8d7434c02916f5e
MaxiSound <maxisound@yandex.ru>
2022-08-19 12:41:20 +0700

    Наконец-то и третью задачу закончил

---
+++ Homework03/third_task/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int counter = 1;
+  int length = cube.Length;
+  while (counter <  length)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll)
+{
+  int counter = coll.Length;
+  int index = 1;
+  while(index < counter)
+  {
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
