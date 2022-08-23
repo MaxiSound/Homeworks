89a91db3880ebeb92e988a7e453315a21aa74986
MaxiSound <maxisound@yandex.ru>
2022-08-23 16:34:10 +0700

    Получается всё что кроме цикла лишнее, и по условию вообще не нужно

--- Homework04/first_task/Program.cs
+++ Homework04/first_task/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+  Console.WriteLine("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+  int Exponentiation(int numberA, int numberB)
+{
+  int result = 1;
+  for(int i=1; i <= numberB; i++) // Вот начало цикла по условию задачи
+  {
+    result = result * numberA;
+  }
+    return result;               // вот конец цикла по условию задачи
+}
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: ");
+  Console.Write(exponentiation);
