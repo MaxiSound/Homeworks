bdbc3ba26d6b10f00ed8dfb3c2e012bc29bb6056
MaxiSound <maxisound@yandex.ru>
2022-08-23 17:29:05 +0700

    вроде всё норм

--- Homework04/second_task/Program.cs
+++ Homework04/second_task/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Sumnumber(int number)
+{
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+    for (int i = 0; i < counter; i++)
+    {
+        advance = number - number % 10;
+        result = result + (number - advance);
+        number = number / 10;
+    }
+    return result;
+}
+int sumnumber = Sumnumber(number);
+Console.WriteLine("Сумма цифр в числе: ");
+Console.Write(sumnumber);
