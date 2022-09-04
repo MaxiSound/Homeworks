705351a3190eb1fead73fb5732c56fc76ac0e519
MaxiSound <maxisound@yandex.ru>
2022-09-04 09:41:02 +0700

    вроде сделал

--- Homework08/first_task/Program.cs
+++ Homework08/first_task/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.WriteLine("Давай придумаем сколько будет строк? ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("А теперь придумаем количество столбцов! ");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine();
+Console.WriteLine("Массив до изменения: ");
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1) - 1; j++)
+    {
+        for (int counter = 0; counter < numbers.GetLength(1) - 1; counter++)
+        {
+            if (numbers[i, counter] < numbers[i, counter + 1]) //для изменения сортировки поменять знак < на противоположный
+            {
+                int temp = 0;
+                temp = numbers[i, counter];
+                numbers[i, counter] = numbers[i, counter + 1];
+                numbers[i, counter + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями");
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ | ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " | ");
+        }
+        Console.Write("]");
+        Console.WriteLine(" ");
+    }
+}
