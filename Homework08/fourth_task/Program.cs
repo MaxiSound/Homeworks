5bdb95e1a92ceafbc31fffa1f361e4b9e8fb46df
MaxiSound <maxisound@yandex.ru>
2022-09-07 07:59:59 +0700

    чуток поправил

--- Homework08/fourth_task/Program.cs
+++ Homework08/fourth_task/Program.cs
@@ -2,7 +2,7 @@
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 
 Console.WriteLine("Введите размеры массива через пробел: ");
-string[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+string[] nums = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
 int[,,] array = GetArray(new int[] { int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2]), }, 10, 99);
 PrintArray(array);
 
