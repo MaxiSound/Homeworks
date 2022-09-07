dd01a254e4a4bcf1a2fa531c79199fc32b44c7a6
MaxiSound <maxisound@yandex.ru>
2022-09-07 18:26:52 +0700

    +

--- Homework09/first_task/Program.cs
+++ Homework09/first_task/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+int m,
+    n;
+Console.WriteLine("Введите число M: ");
+m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+n = Convert.ToInt32(Console.ReadLine());
+
+void PrintRange(int m, int n)
+{
+    if (n > m)
+    {
+        PrintRange(m, n - 1);
+        Console.Write(", ");
+    }
+    Console.Write(n);
+}
+Console.WriteLine("Натуральные числа в данном диапазоне: ");
+PrintRange(m, n);
